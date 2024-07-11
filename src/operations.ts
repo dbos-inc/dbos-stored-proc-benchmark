1b3162717875da38886d04d6d9267ce07a961a5d
Peter Kraft <petereliaskraft@gmail.com>
2024-07-11 12:38:34 -0700

    benchmarked

--- src/operations.ts
+++ src/operations.ts
@@ -9,10 +9,10 @@ export interface dbos_hello {
 
 export class StoredProcBenchmark {
 
-    @GetApi('/sp/:user')
-    static async benchmarkStoredProc(ctxt: HandlerContext, user: string) {
+    @GetApi('/sp/:iterations')
+    static async benchmarkStoredProc(ctxt: HandlerContext, iterations: number) {
         const startTime = performance.now();
-        const output = await ctxt.invoke(StoredProcBenchmark).runStoredProc(user);
+        const output = await ctxt.invoke(StoredProcBenchmark).runStoredProc("dbos", iterations);
         const elapsedTime = performance.now() - startTime;
         return {
             output: output,
@@ -21,17 +21,20 @@ export class StoredProcBenchmark {
     }
 
     @StoredProcedure()
-    static async runStoredProc(ctxt: StoredProcedureContext, user: string): Promise<string> {
-        const query = "INSERT INTO dbos_hello (name, greet_count) VALUES ($1, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-        const { rows } = await ctxt.query<dbos_hello>(query, [user]);
-        const greet_count = rows[0].greet_count;
+    static async runStoredProc(ctxt: StoredProcedureContext, user: string, iterations: number): Promise<string> {
+        let greet_count = null;
+        for (let i = 0; i < iterations; i++) {
+            const query = "INSERT INTO dbos_hello (name, greet_count) VALUES ($1, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
+            const { rows } = await ctxt.query<dbos_hello>(query, [user]);
+            greet_count = rows[0].greet_count;
+        }
         return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
     }
 
-    @GetApi('/txn/:user')
-    static async benchmarkTransaction(ctxt: HandlerContext, user: string) {
+    @GetApi('/txn/:iterations')
+    static async benchmarkTransaction(ctxt: HandlerContext, iterations: number) {
         const startTime = performance.now();
-        const output = await ctxt.invoke(StoredProcBenchmark).runTransaction(user);
+        const output = await ctxt.invoke(StoredProcBenchmark).runTransaction("dbos", iterations);
         const elapsedTime = performance.now() - startTime;
         return {
             output: output,
@@ -40,10 +43,13 @@ export class StoredProcBenchmark {
     }
 
     @Transaction()
-    static async runTransaction(ctxt: TransactionContext<Knex>, user: string): Promise<string> {
-        const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-        const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
-        const greet_count = rows[0].greet_count;
+    static async runTransaction(ctxt: TransactionContext<Knex>, user: string, iterations: number): Promise<string> {
+        let greet_count = null;
+        for (let i = 0; i < iterations; i++) {
+            const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
+            const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
+            greet_count = rows[0].greet_count;
+        }
         return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
     }
 }
